e0720ae66a02fabcffd7c4685be4d6b226ee6265
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2025-06-26 11:09:41 -0400

    Add web changes for fix_annotation_ap_axis.jl

--- web/scripts/web_fix_annotation_ap_axis.jl
+++ web/scripts/web_fix_annotation_ap_axis.jl
@@ -15,20 +15,21 @@ function web_debug_annotation_ap_axis(datasets = datasets)
                 DOM.li(k),
                 DOM.ul(
                     map(collect(keys(datasets[k]))) do i
-                        DOM.li(DOM.a(datasets[k][i].path, href="/debug_annotation_ap_axis/$k/$i"))
+                        DOM.li(DOM.a(datasets[k][i].path, href="/fix_annotation_ap_axis/$k/$i"))
                     end
                 )
             end
         )
     )
     server = Server(
-        "shroff-data.int.janelia.org", 9281;
+        "shroff-data.int.janelia.org", 9381;
         proxy_url="https://shroff-data.int.janelia.org/fix_annotation_ap_axis/"
     )
     route!(server, "/" => App(menu))
     for k in keys(datasets)
         for i in keys(datasets[k])
             route!(server, "/$k/$i" => App(; title="$k[$i]: Shroff C. elegans fix annotation AP axis") do
+                empty!(annotations_cache)
                 return fix_annotation_ap_axis(avg_models, datasets[k][i]; use_myuntwist=true);
             end)
         end
@@ -37,6 +38,7 @@ function web_debug_annotation_ap_axis(datasets = datasets)
 end
 
 function web_main()
+    Threads.@spawn fix_annotation_ap_axis_persist_server()
     WGLMakie.activate!(; resize_to = :body)
     server = web_debug_annotation_ap_axis()
 end
