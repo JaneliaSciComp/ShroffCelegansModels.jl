a81429b4399f7a65b0e383bbcde76745c4db5cc7
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2026-04-17 14:08:29 -0400

    Update loading.jl with config_2026_03_19_v2.json

--- src/demo_averaging/loading.jl
+++ src/demo_averaging/loading.jl
@@ -1,12 +1,13 @@
 #const config_path = raw"D:\shroff\python_model_building\C-Elegans-Model-Generation\config_full.json"
 if gethostname() == "KITTISOPIKULM-2"
-    const config_path = raw"D:\shroff\python_model_building\C-Elegans-Model-Generation\config_2024_09_05_v1.json"
+    const config_path = raw"D:\shroff\python_model_building\C-Elegans-Model-Generation\config_2026_03_19_v2.json"
 elseif gethostname() == "vm7249"
-    const config_path = "/groups/scicompsoft/home/kittisopikulm/src/ShroffCelegansModels.jl/config_2024_09_05_v1.json"
+    const config_path = "/groups/scicompsoft/home/kittisopikulm/src/ShroffCelegansModels.jl/config_2025_12_17_v2.json"
 end
 const voxel_size = 0.1625 # um
 
 using LinearAlgebra
+using JSON3
 LinearAlgebra.BLAS.set_num_threads(12)
 
 @info "Reading Config JSON"
