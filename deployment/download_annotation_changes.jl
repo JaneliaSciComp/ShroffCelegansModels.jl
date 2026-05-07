d113aad4d58d931367727efd3cefae7acf95c4b3
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2026-05-07 15:56:04 -0400

    Use a local dest in download_annotation_changes.jl

--- deployment/download_annotation_changes.jl
+++ deployment/download_annotation_changes.jl
@@ -5,7 +5,7 @@ function (@main)(args)
 
     pod = strip(read(`oc get pod -n shroff-data -l app=shroff-data -o $("jsonpath={.items[0].metadata.name}")`, String))
 
-    dest = joinpath(@__DIR__, "..", "annotation_changes_$(date).h5")
+    dest = "../annotation_changes_$(date).h5"
 
     run(`oc cp -n shroff-data -c fix-ap-axis $(pod):/data/annotations/annotation_changes.h5 $(dest)`)
 
